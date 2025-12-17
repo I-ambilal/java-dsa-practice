d020d1f093d07be6af6468a9f03043cc723e5bca
Bilal Mahmood <165864157+I-ambilal@users.noreply.github.com>
2025-12-17 13:49:19 +0500

    Implement TwoSum class with main method

---
+++ TwoSum.java
@@ -0,0 +1,7 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums= {2,7,11,15};
+        int target=9;
+        System.out.println(nums[0]+", "+nums[1]);
+    }
+}
