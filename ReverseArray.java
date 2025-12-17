8a602a4fd7b61e2577bc84a28415dd0a7ec46e76
Bilal Mahmood <165864157+I-ambilal@users.noreply.github.com>
2025-12-17 13:57:37 +0500

    Reverse array elements

---
+++ ReverseArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        for(int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+    }
+}
